abeb5e148657d536e030d543a72ffaf5619b0452
macEl <laguitanelcharis@gmail.com>
2022-12-18 17:24:58 +0800

    view quizzes draft

--- ClientHTML/teachermodule/teacherQuizzes.php
+++ ClientHTML/teachermodule/teacherQuizzes.php
@@ -12,7 +12,9 @@
 <?php
     include "config.php";
 
-
+    $givenQuizSet = "nmqs%";
+    $quiz = "SELECT * FROM `quiz_list` WHERE `quiz_set` LIKE '$givenQuizSet'";
+    $quizResults = $connection->query($quiz);
 ?>
 
 <body>
@@ -44,7 +46,26 @@
         </div>
         <div class="QuizList">
             <div class="quizzes" id="quizzes">
-                
+                <?php
+                    
+
+                    if ($quizResults->num_rows > 0) {
+                        while ($row = $quizResults->fetch_assoc()) {
+                            echo "<div class='left_Section'>";
+                            echo "<h2 class='Subject'>" . $row['q_name'] . "</h2>";
+                            echo "<h3 class='Quiz_title'>" . $row['q_name'] . "</h3>";
+                            echo "<div class='date'>";
+                            echo "<h3 class='datePosted'> Date Posted: " . $row['start_quiz'] . "</h2>";
+                            echo "<h3 class='dateQuiz'> Date Due: " . $row['end_quiz'] . "</h2>";
+                            echo "</div>";
+                            echo "<div class='right_Section'>";
+                            echo "<button class='listUnlist'> Unlist </button>";
+                            echo "<button class='View'> View </button>";
+                            echo "<button class='Edit'> Edit </button>";
+                            echo "</div>";
+                        }
+                    }
+                ?>
             </div>
         </div>
         <form action="teacherCreateQuiz.php" method="POST">
