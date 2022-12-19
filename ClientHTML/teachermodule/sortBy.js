948872b9b6406bea1d43b8513012e0ecad1e8387
mmiiccoo <2212135@slu.edu.ph>
2022-12-19 13:08:45 +0800

    Delete sortBy.js

--- ClientHTML/teachermodule/sortBy.js
+++
@@ -1,6 +0,0 @@
-function sortBy(that) {
-    const content = document.getElementById("quizzes").innerHTML;
-    if (that.value == "subjectNum") {
-        document.getElementById("quizzes").innerHTML = `<?php include "sortNumericalMethods.php"?>` + content;
-    }
-}
