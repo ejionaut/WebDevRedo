576d4be02158b062f68a9118ad6dae595423c9d2
mmiiccoo <2212135@slu.edu.ph>
2022-12-19 20:45:11 +0800

    Update deleteQuizList.php

--- ClientHTML/teachermodule/deleteQuizList.php
+++ ClientHTML/teachermodule/deleteQuizList.php
@@ -2,10 +2,11 @@
 include "config.php";
 $quiz_code = $_GET['id'];
 $query = "DELETE FROM `quiz_list` WHERE `quiz_code` = '$quiz_code'";
+$query2 = "DELETE FROM `quiz_inventory` WHERE `quiz_code` = '$quiz_code'";
 
 if($connection->query($query) === TRUE){
+	$connection->query($query2);
 	header('Location: teacherQuizzes.php');
 }
 
-header('location: teacherQuizzes.php')
 ?>
