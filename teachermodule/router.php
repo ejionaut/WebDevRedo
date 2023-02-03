d4f40d05bd9cffedb899719455d322522b25e06e
mmiiccoo <111960349+mmiiccoo@users.noreply.github.com>
2023-02-03 16:27:45 +0800

    add router.php

---
+++ teachermodule/router.php
@@ -0,0 +1,17 @@
+ <?php
+ 
+ $request = $_SERVER['REQUEST_URI'];
+
+ switch ($request) {
+     case '/':
+         require 'login.php';
+         break;
+     case '':
+         require 'login.php';
+         break;
+     default:
+         http_response_code(404);
+         require 'login.php';
+         break;
+ }
+?>
