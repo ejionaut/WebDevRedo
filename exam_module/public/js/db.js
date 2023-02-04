31a79d69e97ee8ec02adfa50dc5c47c463b88f21
juniboi <gahidjunroy@gmail.com>
2023-02-04 21:19:09 +0800

    rename db name

--- exam_module/public/js/db.js
+++ exam_module/public/js/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
     password: '',
-    database: 'maindatabase4'
+    database: 'maindatabase'
 })
 
 module.exports = db
