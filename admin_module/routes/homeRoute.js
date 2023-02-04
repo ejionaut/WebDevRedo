be55e4846fff7025265a487e71b12d5dd906b456
juniboi <gahidjunroy@gmail.com>
2023-02-04 21:17:50 +0800

    Delete inital admin_module

--- admin_module/routes/homeRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const { showAdminHome } = require("../public/js/adminHome")
-
-router.get("/home", showAdminHome)
-
-module.exports = router
