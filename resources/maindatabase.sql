cd2dcbcd06727a9a7c397aa0c3fcf404b346c6b6
macEl <laguitanelcharis@gmail.com>
2023-02-04 16:45:16 +0800

    change in db

--- resources/maindatabase.sql
+++ resources/maindatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2022 at 10:17 AM
+-- Generation Time: Feb 04, 2023 at 09:43 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.6
 
@@ -27,7 +27,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `acc_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
@@ -53,40 +52,11 @@ INSERT INTO `accounts` (`acc_id`, `user_id`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history`
---
-
-DROP TABLE IF EXISTS `history`;
-CREATE TABLE `history` (
-  `history_id` text DEFAULT NULL,
-  `sq_array` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `history`
---
-
-INSERT INTO `history` (`history_id`, `sq_array`) VALUES
-('QH0001', ''),
-('QH0002', ''),
-('QH0003', ''),
-('QH0004', ''),
-('QH0005', ''),
-('QH0006', ''),
-('QH0007', ''),
-('QH0008', ''),
-('QH0009', ''),
-('QH0010', '');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `quiz_inventory`
 --
 
-DROP TABLE IF EXISTS `quiz_inventory`;
 CREATE TABLE `quiz_inventory` (
-  `quiz_set` text DEFAULT NULL,
+  `quiz_code` varchar(45) DEFAULT NULL,
   `type_of_quiz` text DEFAULT NULL,
   `question` text DEFAULT NULL,
   `choices` text DEFAULT NULL,
@@ -98,7 +68,7 @@ CREATE TABLE `quiz_inventory` (
 -- Dumping data for table `quiz_inventory`
 --
 
-INSERT INTO `quiz_inventory` (`quiz_set`, `type_of_quiz`, `question`, `choices`, `answer`, `points`) VALUES
+INSERT INTO `quiz_inventory` (`quiz_code`, `type_of_quiz`, `question`, `choices`, `answer`, `points`) VALUES
 ('tpqs001', 'iden', 'It is the process of generating meaning by sending and receiving verbal and non-verbal symbols and signs that are influenced by multiple contexts.', '', 'communication', 1),
 ('tpqs001', 'iden', 'It is a structured communication based on the actual audience\'s needs in order to achieve a certain purpose.', '', 'presentation', 1),
 ('tpqs001', 'iden', 'A type of presentation that influences a change in the belief, attitude, or behavior of the audience', '', 'persuasive presentation', 1),
@@ -137,14 +107,6 @@ INSERT INTO `quiz_inventory` (`quiz_set`, `type_of_quiz`, `question`, `choices`,
 ('seqs001', 'mc', 'A software development that uses an iterative approach, in which the product is developed in increments that gradually increase the implemented functionality.', 'Evolutionary Development,Incremental Model,Spiral Development Model,Cleanroom Model', 'Cleanroom Model', 1),
 ('seqs001', 'enum', 'Enumerate the Goals of Software Development efforts.', '', 'The right feature set,High quality,Short time to market', 3),
 ('seqs001', 'enum', 'Give at least four traits of Agile Teams.', '', 'Competence,Common Focus,Collaboration,Mutual trust and respect,Decision-making ability,Self-organization,Fuzzy-problem solving ability', 7),
-('adqs001', 'iden', 'Used to communicate with a machine and instruct it what to do.', '', 'Programming Language', 1),
-('adqs001', 'iden', 'Disciplined technique for restructuring an existing body of code, altering its internal structure without changing its external behavior.', '', 'Code Refactoring', 1),
-('adqs001', 'iden', 'Refers to the gradual deterioration of software over time or its decreasing inability to respond to its changing environment.', '', 'Software Decay', 1),
-('adqs001', 'iden', 'Involves finding and removing bugs (problem that prevent correct operation of the program).', '', 'Debugging', 1),
-('adqs001', 'iden', 'Process of assessing the functionality and correctness of a computer program through execution or analysis.', '', 'Testing', 1),
-('adqs001', 'mc', 'Translate the source code (written in a programming language) into the computer language (object code).', 'Compiler,Interpreter,Assembler,Decompiler', 'Compiler', 1),
-('adqs001', 'mc', 'Reverse-engineer the translation performed by compilers and assemblers.', 'Compiler,Interpreter,Assembler,Decompiler', 'Decompiler', 1),
-('adqs001', 'mc', 'Used in assembly language to convert the source code into the machine language.', 'Compiler,Interpreter,Assembler,Decompiler', 'Assembler', 1),
 ('adqs001', 'enum', 'Enumerate the Four Pillars of Object Oriented Programming Paradigm.', '', 'Abstraction,Inheritance,Polymorphism,Encapsulation', 4),
 ('adqs001', 'enum', 'Enumerate the 2 types of Declarative Programming.', '', 'Logical Programming,Functional Programming', 2),
 ('wdqs001', 'mc', 'Global system of interconnected computer networks that communicate and interchange data through TCP/IP.', 'Network,World Wide Web,Internet,LAN', 'Internet', 1),
@@ -158,7 +120,12 @@ INSERT INTO `quiz_inventory` (`quiz_set`, `type_of_quiz`, `question`, `choices`,
 ('wdqs001', 'tf', 'As part of the HTML coding conventions, use lowercase characters for ALL element and attribute names.', 'true,false', 'TRUE', 1),
 ('wdqs001', 'tf', 'ACSS specifications are maintained and published by WHATWG.', 'true,false', 'FALSE', 1),
 ('wdqs001', 'tf', 'Pseudo-elements allow selection of elements based on their structural relationships with other elements in the document tree.', 'true,false', 'FALSE', 1),
-('wdqs001', 'enum', 'Enumerate the 3 different sources (a.k.a. origin) of styles applied to HTML documents.', '', 'User agent styles,User styles,Author styles', 3);
+('wdqs001', 'enum', 'Enumerate the 3 different sources (a.k.a. origin) of styles applied to HTML documents.', '', 'User agent styles,User styles,Author styles', 3),
+('adqs001', 'mc', 'question1', 'hi,hello,how are you,kumusta', 'hello', 1),
+('adqs001', 'mc', 'question2', 'hi,hello,how are you,kumusta', 'hello', 1),
+('adqs001', 'mc', 'question3', 'hi,hello,how are you,kumusta', 'hello', 1),
+('adqs001', 'mc', 'question4', 'hi,hello,how are you,kumusta', 'hello', 1),
+('adqs001', 'mc', 'question5', 'hi,hello,how are you,kumusta', 'hello', 1);
 
 -- --------------------------------------------------------
 
@@ -166,32 +133,25 @@ INSERT INTO `quiz_inventory` (`quiz_set`, `type_of_quiz`, `question`, `choices`,
 -- Table structure for table `quiz_list`
 --
 
-DROP TABLE IF EXISTS `quiz_list`;
 CREATE TABLE `quiz_list` (
-  `quiz_code` text DEFAULT NULL,
   `q_name` text DEFAULT NULL,
   `q_password` text DEFAULT NULL,
-  `quiz_set` text DEFAULT NULL,
-  `subject_code` text DEFAULT NULL,
+  `quiz_code` text DEFAULT NULL,
   `q_display_setting` text DEFAULT NULL,
-  `start_quiz` text DEFAULT NULL,
-  `end_quiz` text DEFAULT NULL
+  `total_score` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `quiz_list`
 --
 
-INSERT INTO `quiz_list` (`quiz_code`, `q_name`, `q_password`, `quiz_set`, `subject_code`, `q_display_setting`, `start_quiz`, `end_quiz`) VALUES
-('tp001a', 'TechPres Prelims Quiz# 1', 'tp001a', 'tpqs001', 'tp', 'listed', '', ''),
-('nm001a', 'NumMeth Prelims Quiz# 1', 'nm001a', 'nmqs001', 'nm', 'listed', '', ''),
-('pd001a', 'PerDev Prelims Quiz# 1', 'pd001a', 'pdqs001', 'pd', 'listed', '', ''),
-('se001a', 'SoftEng Prelims Quiz# 1', 'seqs001a', 'seqs001', 'se', 'listed', '', ''),
-('ad001a', 'AppDev Prelims Quiz# 1', 'adqs001a', 'adqs001', 'ad', 'listed', '', ''),
-('wd001a', 'WebDev Prelims Quiz# 1', 'wdqs001a', 'wdqs001', 'wd', 'unlisted', '', ''),
-('', '', '', '', '', '', '', ''),
-('', '', '', '', '', '', 'q_display_setting - whether or not the teacher wants to display the quiz or not', ''),
-('', '', '', '', '', '', 'student_list - array, list of students allowed to take the quiz', '');
+INSERT INTO `quiz_list` (`q_name`, `q_password`, `quiz_code`, `q_display_setting`, `total_score`) VALUES
+('TechPres Prelims Quiz# 1', 'tp001a', 'tpqs001', 'listed', 21),
+('NumMeth Prelims Quiz# 1', 'nm001a', 'nmqs001', 'listed', 12),
+('PerDev Prelims Quiz# 1', 'pd001a', 'pdqs001', 'listed', 18),
+('SoftEng Prelims Quiz# 1', 'seqs001a', 'seqs001', 'listed', 18),
+('AppDev Prelims Quiz# 1', 'adqs001a', 'adqs001', 'listed', 11),
+('WebDev Prelims Quiz# 1', 'wdqs001a', 'wdqs001', 'unlisted', 14);
 
 -- --------------------------------------------------------
 
@@ -199,31 +159,27 @@ INSERT INTO `quiz_list` (`quiz_code`, `q_name`, `q_password`, `quiz_set`, `subje
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
-  `user_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
   `lastname` text DEFAULT NULL,
-  `firstname` text DEFAULT NULL,
-  `history_id` text DEFAULT NULL,
-  `avail_quizzes` text DEFAULT NULL,
-  `subject_list` text DEFAULT NULL
+  `firstname` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`user_id`, `lastname`, `firstname`, `history_id`, `avail_quizzes`, `subject_list`) VALUES
-(2022001, 'Johnson', 'Dwayne', 'QH0001', '', 'nm,tp,wd'),
-(2022002, 'Wilson', 'Troy', 'QH0002', '', 'ad,se,nm'),
-(2022003, 'Smith', 'Sam', 'QH0003', '', 'ad,pd,tp'),
-(2022004, 'Williams', 'Robert', 'QH0004', '', 'tp,wd,ad'),
-(2022005, 'Davis', 'John', 'QH0005', '', 'se,pd,tp'),
-(2022006, 'Garcia', 'Giselle', 'QH0006', '', 'wd,pd,ad'),
-(2022007, 'Jones', 'Indianna', 'QH0007', '', 'nm,se,pd'),
-(2022008, 'Browne', 'Jake', 'QH0008', '', 'pd,se,wd'),
-(2022009, 'Moore', 'Mandy', 'QH0009', '', 'ad,wd,tp'),
-(2022010, 'Miller', 'Gilbert', 'QH0010', '', 'nm,tp,ad');
+INSERT INTO `student` (`user_id`, `lastname`, `firstname`) VALUES
+(2022001, 'Johnson', 'Dwayne'),
+(2022002, 'Wilson', 'Troy'),
+(2022003, 'Smith', 'Sam'),
+(2022004, 'Williams', 'Robert'),
+(2022005, 'Davis', 'John'),
+(2022006, 'Garcia', 'Giselle'),
+(2022007, 'Jones', 'Indianna'),
+(2022008, 'Browne', 'Jake'),
+(2022009, 'Moore', 'Mandy'),
+(2022010, 'Miller', 'Gilbert');
 
 -- --------------------------------------------------------
 
@@ -231,12 +187,11 @@ INSERT INTO `student` (`user_id`, `lastname`, `firstname`, `history_id`, `avail_
 -- Table structure for table `student_quiz`
 --
 
-DROP TABLE IF EXISTS `student_quiz`;
 CREATE TABLE `student_quiz` (
   `user_id` bigint(20) DEFAULT NULL,
   `quiz_code` text DEFAULT NULL,
   `score` bigint(20) DEFAULT NULL,
-  `sq_id` text DEFAULT NULL,
+  `sq_id` int(11) NOT NULL,
   `timestamp` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -245,23 +200,54 @@ CREATE TABLE `student_quiz` (
 --
 
 INSERT INTO `student_quiz` (`user_id`, `quiz_code`, `score`, `sq_id`, `timestamp`) VALUES
-(2022001, 'tp001a', 10, NULL, NULL),
-(2022003, 'tp001a', 4, NULL, NULL);
+(2022001, 'tpqs001', 10, 1, NULL),
+(2022003, 'tpqs001', 4, 2, NULL),
+(2022001, 'pdqs001', 0, 7, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student_quiz_answer`
+-- Table structure for table `student_quiz_details`
 --
 
-DROP TABLE IF EXISTS `student_quiz_answer`;
-CREATE TABLE `student_quiz_answer` (
-  `acc_id` bigint(20) DEFAULT NULL,
-  `sq_id` text DEFAULT NULL,
-  `quiz_code` text DEFAULT NULL,
-  `question` text DEFAULT NULL,
-  `answer` text DEFAULT NULL
+CREATE TABLE `student_quiz_details` (
+  `sq_id` int(11) NOT NULL,
+  `quiz_code` varchar(45) DEFAULT NULL,
+  `question` varchar(45) DEFAULT NULL,
+  `answer` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `student_quiz`
+--
+ALTER TABLE `student_quiz`
+  ADD PRIMARY KEY (`sq_id`);
+
+--
+-- Indexes for table `student_quiz_details`
+--
+ALTER TABLE `student_quiz_details`
+  ADD PRIMARY KEY (`sq_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_quiz`
+--
+ALTER TABLE `student_quiz`
+  MODIFY `sq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
