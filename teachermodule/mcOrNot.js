7ae987ff8279a234d2eba23a4864d4d50af37436
macEl <laguitanelcharis@gmail.com>
2023-02-08 09:09:59 +0800

    last na ata

--- teachermodule/mcOrNot.js
+++ teachermodule/mcOrNot.js
@@ -5,10 +5,9 @@ function mcOrNot(that) {
         document.getElementById("ifYesInput").style.display = "block";
         document.getElementById("ifYesInput").setAttribute("required", "");
         document.getElementById("ifYesInput").removeAttribute("disabled");
-    } else if (that.value = "tf"){
+    } else if (that.value == "tf"){
         document.getElementById("ifYesInput").value = "true,false";
-    }
-    else {
+    } else if (that.value == "enum" || that.value == "iden") {
         document.getElementById("ifYesInput").value = "";
         document.getElementById("ifYesInput").setAttribute("disabled", "");
         document.getElementById("ifYesInput").setAttribute("value", "");
